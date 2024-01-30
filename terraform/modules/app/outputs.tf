42e8ecf63a105755b11321b9ca9475fd273e7721
Miguel Ortiz <miguel.ortiz.esp@gmail.com>
2024-01-30 09:11:02 -0600

    Run terraform fmt

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -1,6 +1,6 @@
 
 output "app_load_balancer_endpoint" {
-   value       = data.kubernetes_service.app.status.0.load_balancer.0.ingress.0.ip
-#  value       = data.kubernetes_service.app
+  value = data.kubernetes_service.app.status.0.load_balancer.0.ingress.0.ip
+  #  value       = data.kubernetes_service.app
   description = "Load Balancer for app deployed."
 }
