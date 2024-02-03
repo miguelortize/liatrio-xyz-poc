2f948e009e62df6a8baed743c1e20554734b727b
Miguel Ortiz <miguel.ortiz.esp@gmail.com>
2024-02-03 14:39:28 -0600

    Create new version

--- microservice/app/src/test/java/com/example/springboot/HelloControllerTest.java
+++ microservice/app/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -18,7 +18,7 @@ public class HelloControllerTest {
     public void shouldReturnDefaultMessage() throws Exception {
         mockMvc.perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.message").value("Automate all the things!"))
+            .andExpect(jsonPath("$.message").value("Automate all the things! V2.0"))
             .andExpect(jsonPath("$.timestamp").isNumber());
     }
 }
